b0f7d3bc3453194dcc8201985788e250a69cfda6
KatrinkaYu <katrinkayu@mail.ru>
2016-07-19 14:11:54 +0300

    finish_task2_5

--- test/add_contact_task1.py
+++ test/add_contact_task1.py
@@ -12,9 +12,9 @@ def app(request):
 
 def test_add_contact_task1(app):
         app.session.login(username="admin", password="secret")
-        app.create_contact(Contact(name="new_contact", middle_name="contact_middle_name", last_name="contact_last_name",
-                       nickname="contact_nickname", title="contact_title", company="contact_company",
-                       address="contact_address", home_telephone="123456", mobile_telephone="234567", work_telephone="345678",
-                       email="mail@qwerty.com", year="2000", address2="address", phone2="12"))
+        app.contact.create(Contact(name="new_contact", middle_name="contact_middle_name", last_name="contact_last_name",
+                                   nickname="contact_nickname", title="contact_title", company="contact_company",
+                                   address="contact_address", home_telephone="123456", mobile_telephone="234567", work_telephone="345678",
+                                   email="mail@qwerty.com", year="2000", address2="address", phone2="12"))
         app.session.logout()
 
--- test/add_group_task1.py
+++ test/add_group_task1.py
@@ -12,10 +12,10 @@ def app(request):
 
 def test_add_group_task1(app):
         app.session.login(username="admin", password="secret")
-        app.create_group(Group(name="group_task1", header="new_group", footer="new_group"))
+        app.group.create(Group(name="group_task1", header="new_group", footer="new_group"))
         app.session.logout()
 
 def test_add_empty_group_task1(app):
         app.session.login(username="admin", password="secret")
-        app.create_group(Group(name="", header="", footer=""))
+        app.group.create(Group(name="", header="", footer=""))
         app.session.logout()
